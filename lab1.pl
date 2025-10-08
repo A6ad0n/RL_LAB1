c4a6fa15143ce2174271087353f30e6d1310dff6
Egor Bazakin <egor.bazakin.09@gmail.com>
2025-10-08 14:03:12 +0300

    Lab1

---
+++ lab1.pl
@@ -0,0 +1,17 @@
+is_sorted(Pred, List) :-
+    callable(Pred),
+    is_list(List),
+    is_sorted_impl(Pred, List), !.
+is_sorted(_, _) :-
+    fail.
+
+is_sorted_impl(_, []) :- !.
+is_sorted_impl(_, [_]) :- !.
+is_sorted_impl(Pred, [A,B|T]) :-
+    call(Pred, A, B), !,
+    is_sorted_impl(Pred, [B|T]).
+is_sorted_impl(_, _) :-
+    fail.
+
+is_sorted(List) :-
+    is_sorted(<, List), !.
